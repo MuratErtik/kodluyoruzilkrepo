e9e090e37c158c08474a57daad896a204a494a2b
Murat Ertik <dmuratertik1@gmail.com>
2026-04-22 14:02:27 +0300

    PalindromeChecker done.

---
+++ demo/src/main/java/org/demo/java101/PalindromeChecker.java
@@ -0,0 +1,18 @@
+package org.demo.java101;
+
+public class PalindromeChecker {
+
+    public boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+
+}
