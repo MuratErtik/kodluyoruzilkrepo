d6d63130fabd526cede3acf870df58f225dd1544
Murat Ertik <dmuratertik1@gmail.com>
2026-04-22 11:45:47 +0300

    ReverseTriangle done.

---
+++ demo/src/main/java/org/demo/java101/ReverseTriangle.java
@@ -0,0 +1,24 @@
+package org.demo.java101;
+
+import java.util.Scanner;
+
+public class ReverseTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Basamak Sayısı: ");
+        int n = scanner.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
